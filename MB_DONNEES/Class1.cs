b38911466f1101c83168127dae90291ccf4f53d9
DIA Moad <DIA>
2024-03-25 11:23:08 +0100

    Multiburo

---
+++ MB_DONNEES/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB_DONNEES
+{
+    public class Class1
+    {
+    }
+}
